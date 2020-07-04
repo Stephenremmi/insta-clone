d31c579b063699ab40708b46bcc62c2f753f5642
stephen remmi <stephenremmi@gmail.com>
2020-07-04 11:48:46 +0300

    Add python-decouple package

--- insta/admin.py
+++ insta/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post, Comment, UserProfile
+
+class ProfileAdmin(admin.ModelAdmin):
+    filter_horizontal =("followers", "following",)
 
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(UserProfile, admin_class=ProfileAdmin)
+
