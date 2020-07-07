ab9f3a48a3fc4d116d5c259b39bd721fae6f788f
stephen remmi <stephenremmi@gmail.com>
2020-07-07 13:55:16 +0300

    Tweak code in base and registration_form templates

--- insta_project/urls.py
+++ insta_project/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from insta import views
+from django.contrib.auth import views as auth_views 
+from django_registration.backends.one_step.views import RegistrationView 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/register/',
+        RegistrationView.as_view(success_url='/accounts/login/'),
+        name='django_registration_register'),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    #url('accounts/', include('django_registration.backends.activation.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
