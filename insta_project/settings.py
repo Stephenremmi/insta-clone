0d075e62cea1880ecffa99472e6f34ee71dc4ee7
stephen remmi <stephenremmi@gmail.com>
2020-07-07 14:08:57 +0300

    Add urls in url module

--- insta_project/settings.py
+++ insta_project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'insta',
-    'bootstrap3'
+    'bootstrap3',
+    'django_registration'
 ]
 
 MIDDLEWARE = [
